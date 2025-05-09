474d37b64b48c9df9f361698b2dd9d49fd888ba2
Ryland Beard <ryland@Rylands-MBP-6.lan>
2025-05-09 14:59:57 -0500

    Use environment variables for Plaid config and prepare for secure deployment

--- src/lib/plaid.ts
+++ src/lib/plaid.ts
@@ -1,8 +1,8 @@
 import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid'
 
-const PLAID_CLIENT_ID = '681695fcc1a0a80023f09b28'
-const PLAID_SECRET = '8e14167b5941f096677c0efe960376'
-const PLAID_ENV = 'sandbox'  // or 'development' or 'production'
+const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID!
+const PLAID_SECRET = process.env.PLAID_SECRET!
+const PLAID_ENV = (process.env.PLAID_ENV as 'sandbox' | 'development' | 'production') || 'sandbox'
 
 const config = new Configuration({
   basePath: PlaidEnvironments[PLAID_ENV],
